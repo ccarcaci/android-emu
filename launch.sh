6bb6385e794ba006b0175c3e4767a7f68fae7a30
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-02-16 15:57:41 +0100

    Take current path and install

--- launch.sh
+++ launch.sh
@@ -1,17 +1,20 @@
-export sdkTools=sdk-tools-linux-4333796
 export JAVA_HOME=/home/claudio/Downloads/apps/jdk1.8.0_201
-export ANDROID_SDK_ROOT=/home/claudio/Downloads/git/android-emu/sdk-tools/$sdkTools
-export version=25
 
-if [ ! -d "sdk-tools/$sdkTools" ]; then
+baseDir=$(dirname "$0")
+sdkTools=sdk-tools-linux-4333796
+sdkPath=sdk-tools/$sdkTools
+version=25
+export ANDROID_SDK_ROOT=$baseDir/$sdkPath
+
+if [ ! -d "$sdkPath" ]; then
   wget -P sdk-tools https://dl.google.com/android/repository/$sdkTools.zip
-  unzip sdk-tools/$sdkTools.zip -d sdk-tools/$sdkTools
-  rm sdk-tools/$sdkTools.zip
+  unzip $sdkPath.zip -d $sdkPath
+  rm $sdkPath.zip
 
-  yes | ./sdk-tools/$sdkTools/tools/bin/sdkmanager "platform-tools" "platforms;android-$version" "emulator" "system-images;android-$version;google_apis;x86"
+  yes | $baseDir/$sdkPath/tools/bin/sdkmanager "platform-tools" "platforms;android-$version" "emulator" "system-images;android-$version;google_apis;x86"
 touch /home/claudio/.android/repositories.cfg
-  ./sdk-tools/$sdkTools/tools/bin/avdmanager create avd --name test --package "system-images;android-$version;google_apis;x86" --device "Nexus 5X"
+  $baseDir/$sdkPath/tools/bin/avdmanager create avd --name test --package "system-images;android-$version;google_apis;x86" --device "Nexus 5X"
 fi
 
-./sdk-tools/$sdkTools/tools/bin/sdkmanager --update
-exec ./sdk-tools/$sdkTools/tools/emulator @test > /dev/null &
+$baseDir$sdkPath/tools/bin/sdkmanager --update
+exec $baseDir/$sdkPath/tools/emulator @test > /dev/null &
