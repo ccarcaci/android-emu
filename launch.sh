b500b4c8b28554b22b6c69a99cca35e615caac22
Claudio Carcaci <claudiocarcaci@gmail.com>
2019-04-24 20:04:53 +0200

    Use absolute path to download the sdk-tools into android-emu folder

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 baseDir=$(dirname "$0")
 sdkTools=sdk-tools-linux-4333796
-sdkPath=sdk-tools/$sdkTools
+sdkPath=$baseDir/sdk-tools/$sdkTools
 version=28
 
-export ANDROID_SDK_ROOT=$baseDir/$sdkPath
+export ANDROID_SDK_ROOT=$sdkPath
 
 if [ -z "$JAVA_HOME" ]; then
   echo "JAVA_HOME not found"
@@ -15,11 +15,11 @@ if [ ! -d "$sdkPath" ]; then
   unzip $sdkPath.zip -d $sdkPath
   rm $sdkPath.zip
 
-  yes | $baseDir/$sdkPath/tools/bin/sdkmanager "platform-tools" "platforms;android-$version" "emulator" "system-images;android-$version;google_apis;x86"
+  yes | $sdkPath/tools/bin/sdkmanager "platform-tools" "platforms;android-$version" "emulator" "system-images;android-$version;google_apis;x86"
 touch /home/claudio/.android/repositories.cfg
-  $baseDir/$sdkPath/tools/bin/avdmanager delete avd --name test
-  $baseDir/$sdkPath/tools/bin/avdmanager create avd --name test --package "system-images;android-$version;google_apis;x86" --device "Nexus 5X"
+  $sdkPath/tools/bin/avdmanager delete avd --name test
+  $sdkPath/tools/bin/avdmanager create avd --name test --package "system-images;android-$version;google_apis;x86" --device "Nexus 5X"
 fi
 
-$baseDir/$sdkPath/tools/bin/sdkmanager --update
-exec $baseDir/$sdkPath/emulator/emulator @test > /dev/null &
+$sdkPath/tools/bin/sdkmanager --update
+exec $sdkPath/emulator/emulator @test > /dev/null &
