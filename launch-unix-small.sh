ad74da8832811a2e040045527a87d5c1ea171495
Claudio Carcaci <claudiocarcaci@gmail.com>
2020-02-02 22:05:12 +0100

    Add SmallScreen device script to test 3.7 inches screens

---
+++ launch-unix-small.sh
@@ -0,0 +1,42 @@
+baseDir=$(dirname "$0")
+sdkTools=sdk-tools-linux-4333796
+sdkPath=$baseDir/sdk-tools/$sdkTools
+version=29
+
+if [ -z "$JAVA_HOME" ]; then
+  echo "JAVA_HOME not found"
+  exit
+fi
+
+if [ ! -d "$sdkPath" ]; then
+  wget -P sdk-tools https://dl.google.com/android/repository/$sdkTools.zip
+  unzip $sdkPath.zip -d $sdkPath
+  rm $sdkPath.zip
+fi
+
+if [ "$1" == "--reinstall-device" ]; then
+  yes | $sdkPath/tools/bin/sdkmanager "platform-tools" "platforms;android-$version" "emulator" "system-images;android-$version;google_apis_playstore;x86_64"
+  touch $HOME/.android/repositories.cfg
+  $sdkPath/tools/bin/avdmanager delete avd --name "SmallScreen"
+  $sdkPath/tools/bin/avdmanager create avd --name "SmallScreen" --package "system-images;android-$version;google_apis_playstore;x86_64" --device "3.7in WVGA (Nexus One)"
+fi
+
+export ANDROID_SDK_ROOT=$sdkPath
+
+$sdkPath/tools/bin/sdkmanager --update
+exec $sdkPath/emulator/emulator @SmallScreen > /dev/null &
+
+curDateSecs=$(date +%s)
+lastUpdate=$(cat $baseDir/last-update)
+checkUpdatePeriod=$(expr 86400 \* 90) # 90 days
+updateEnding=$(expr $lastUpdate + $checkUpdatePeriod)
+
+if [ "$curDateSecs" -gt "$updateEnding" ]; then
+  echo "*************************************************"
+  echo "* Check sdk-tools-linux and sdk version updates *"
+  echo "* This message will be printed once             *"
+  echo "* Link below                                    *"
+  echo "*************************************************"
+  echo "https://developer.android.com/studio/?gclid=CjwKCAiAjrXxBRAPEiwAiM3DQpEZ4I-DSCnFic2f13Xdt8zdfxUcmi118B8HJNF5zuA-ioSfvws5BBoCW2IQAvD_BwE#command-tools"
+  date +%s > $baseDir/last-update
+fi
